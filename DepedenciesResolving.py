038c54f8187bdec71e624a0efc2f2e0df1b27293
VikiDinkova <vikidinkova94@gmail.com>
2015-11-08 13:28:17 +0200

    The third task

---
+++ DepedenciesResolving.py
@@ -0,0 +1,31 @@
+# The solution is incomplete
+
+def dependencies_resolving(all_packages, key):
+	print('Installing {0}'.format(key))
+	if all_packages[key] == []:
+		print('Installing {0}'.format(key))
+		return
+	line = 'In order to install {0}, we need '.format(key)
+	for item in all_packages[key]:
+		line += ' ' + item + ' '
+	print(line)
+	for value_key in all_packages[key]:
+		return dependencies_resolving(all_packages, value_key)
+
+
+all_packages = {
+				"backbone": ["jQuery", "underscore"],
+				"jQuery": ["queryJ"],
+				"underscore": ["lodash"],
+				"queryJ": [],
+				"lodash": []
+}
+
+dependencies = {
+				"projectName": "Test0000",
+				"dependencies": ["backbone"]
+}
+
+for package in dependencies['dependencies']:
+	dependencies_resolving(all_packages, package)
+print('All done!')
