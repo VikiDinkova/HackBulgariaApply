3ca026a7b02a3aab9399d53fc5631e3c043890ea
VikiDinkova <vikidinkova94@gmail.com>
2015-11-09 16:55:23 +0200

    fixed recursion

--- DepedenciesResolving.py
+++ DepedenciesResolving.py
@@ -1,17 +1,15 @@
-# The solution is incomplete
-
 def dependencies_resolving(all_packages, key):
 	print('Installing {0}'.format(key))
 	if all_packages[key] == []:
-		print('Installing {0}'.format(key))
 		return
 	line = 'In order to install {0}, we need '.format(key)
 	for item in all_packages[key]:
 		line += ' ' + item + ' '
 	print(line)
 	for value_key in all_packages[key]:
-		return dependencies_resolving(all_packages, value_key)
-
+		dependencies_resolving(all_packages, value_key)
+	return 
+		
 
 all_packages = {
 				"backbone": ["jQuery", "underscore"],
